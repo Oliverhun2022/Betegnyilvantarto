f374e9af1eebe604e72d0356099bb8b7442ab26b
bencepapp09 <112862119+bencepapp09@users.noreply.github.com>
2024-02-16 10:24:26 +0100

    Add files via upload

---
+++ adatbázis/paciens.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2024. Feb 16. 10:22
+-- Kiszolgáló verziója: 10.4.24-MariaDB
+-- PHP verzió: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `projekt`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `paciens`
+--
+
+CREATE TABLE `paciens` (
+  `ID` int(9) NOT NULL,
+  `nev` varchar(12) COLLATE utf8_hungarian_ci NOT NULL,
+  `eletkor` int(2) DEFAULT NULL,
+  `neme` varchar(6) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `szuletesi_datum` date DEFAULT NULL,
+  `foglalkozas` varchar(10) COLLATE utf8_hungarian_ci NOT NULL,
+  `mobiltelefon_szama` int(10) DEFAULT NULL,
+  `telefonszama` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `paciens`
+--
+
+INSERT INTO `paciens` (`ID`, `nev`, `eletkor`, `neme`, `szuletesi_datum`, `foglalkozas`, `mobiltelefon_szama`, `telefonszama`) VALUES
+(1300528, 'Mezőfi Tamás', 30, 'férfi', '1989-02-23', 'tanuló', 62093360, 670130215),
+(1800309, 'Ferenczi Ist', 65, 'nő', '1960-03-28', 'szakács', 670194650, 620210135),
+(2001510, 'Petőfi Sándo', 40, 'férfi', '1979-04-28', 'író', 2147483647, 2147483647),
+(2345610, 'Kovács Béla', 45, 'férfi', '1978-03-15', 'asztalos', 620765130, 630158630),
+(4501560, 'Kováts Mátyá', 30, 'férfi', '1976-05-30', 'festő', 2147483647, 630110900);
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `paciens`
+--
+ALTER TABLE `paciens`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `foglalkozas` (`foglalkozas`),
+  ADD UNIQUE KEY `foglalkozas_2` (`foglalkozas`),
+  ADD KEY `nev` (`nev`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
