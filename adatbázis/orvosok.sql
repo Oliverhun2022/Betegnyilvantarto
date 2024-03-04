19b5ab23dabeb8d0af5f1771504c68446cf5f0a5
bencepapp09 <112862119+bencepapp09@users.noreply.github.com>
2024-03-04 08:40:25 +0100

    Add files via upload

---
+++ adatbázis/orvosok.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2024. Már 04. 08:34
+-- Kiszolgáló verziója: 10.4.28-MariaDB
+-- PHP verzió: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `projekt_2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `orvosok`
+--
+
+CREATE TABLE `orvosok` (
+  `id` int(11) NOT NULL,
+  `elso_nev` varchar(15) DEFAULT NULL,
+  `utolso_nev` varchar(15) DEFAULT NULL,
+  `iranyitoszam` int(11) DEFAULT NULL,
+  `cim` text DEFAULT NULL,
+  `telefon` int(11) DEFAULT NULL,
+  `egyalkalmas_jelszo` int(11) DEFAULT NULL,
+  `letrehoz_datuma` date DEFAULT NULL,
+  `frissit_datuma` date DEFAULT NULL,
+  `varosId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `orvosok`
+--
+
+INSERT INTO `orvosok` (`id`, `elso_nev`, `utolso_nev`, `iranyitoszam`, `cim`, `telefon`, `egyalkalmas_jelszo`, `letrehoz_datuma`, `frissit_datuma`, `varosId`) VALUES
+(1, 'dr.Bognár', 'Virág', 1014, 'Hegedűs Gyula utca 33.', 2147483647, 12345, '2023-06-19', '2023-06-26', 0),
+(2, 'dr.Balogh', 'Orsolya', 1017, 'Szent István körút 18.', 620220410, 23412, '2023-05-21', '2023-05-28', 0),
+(3, 'dr.Szabó', 'Zoltán', 1030, 'Boldog Sándor István körút 1-3.', 620310200, 98764, '2023-06-21', '2023-06-28', 0),
+(4, 'dr.Tóth', 'Réka', 1017, 'Munkácsy Mihály utca 15.', 620320195, 64556, '2023-07-20', '2023-07-27', 0),
+(5, 'dr.Tóbiás', 'Dávid', 1017, 'Szent István körút 18.', 620320195, 23412, '2023-04-21', '2023-04-28', 0);
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `orvosok`
+--
+ALTER TABLE `orvosok`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- adatbázis/szolgaltatasok.sql
+++ adatbázis/szolgaltatasok.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2024. Feb 27. 10:50
+-- Létrehozás ideje: 2024. Már 04. 08:34
 -- Kiszolgáló verziója: 10.4.28-MariaDB
 -- PHP verzió: 8.2.4
 
@@ -29,8 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `szolgaltatasok` (
   `id` int(11) NOT NULL,
-  `parodontalis_betegseg` varchar(255) DEFAULT NULL,
-  `fenykep` varchar(255) DEFAULT NULL,
+  `kezeles` varchar(255) DEFAULT NULL,
   `letrehozas_datum` date DEFAULT NULL,
   `frissites_datum` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
@@ -39,12 +38,12 @@ CREATE TABLE `szolgaltatasok` (
 -- A tábla adatainak kiíratása `szolgaltatasok`
 --
 
-INSERT INTO `szolgaltatasok` (`id`, `parodontalis_betegseg`, `fenykep`, `letrehozas_datum`, `frissites_datum`) VALUES
-(11023, 'halitózis', 'extraorális fotózás', '2024-02-10', '2024-02-13'),
-(20112, 'foglazulás', 'digitális fotózás', '2024-01-17', '2024-01-22'),
-(20321, 'fogvesztés', 'intraorális fotózás', '2024-01-23', '2024-01-26'),
-(32110, 'fogíny duzzanat', 'szájüregi fotózás', '2024-01-15', '2024-01-20'),
-(57199, 'ínyvérzés', 'dental fotózás', '2023-12-09', '2023-12-16');
+INSERT INTO `szolgaltatasok` (`id`, `kezeles`, `letrehozas_datum`, `frissites_datum`) VALUES
+(1, 'halitózis', '2024-02-10', '2024-02-13'),
+(2, 'foglazulás', '2024-01-17', '2024-01-22'),
+(3, 'fogvesztés', '2024-01-23', '2024-01-26'),
+(4, 'fogíny duzzanat', '2024-01-15', '2024-01-20'),
+(5, 'ínyvérzés', '2023-12-09', '2023-12-16');
 
 --
 -- Indexek a kiírt táblákhoz
