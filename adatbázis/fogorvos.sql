c1d6463824003929c4d109a4c979a8231a059624
bencepapp09 <112862119+bencepapp09@users.noreply.github.com>
2024-03-18 12:59:37 +0100

    Update fogorvos.sql

--- adatbázis/fogorvos.sql
+++ adatbázis/fogorvos.sql
@@ -27,30 +27,29 @@ SET time_zone = "+00:00";
 -- Tábla szerkezet ehhez a táblához `fogorvos_osszevon`
 --
 
-CREATE TABLE `fogorvos_osszevon` (
-  `fogorvos_azon` int(8) NOT NULL,
-  `Mobiltelefonszam` int(10) DEFAULT NULL,
-  `Neve` varchar(50) COLLATE utf8_hungarian_ci DEFAULT NULL,
-  `Email_cim` varchar(50) COLLATE utf8_hungarian_ci DEFAULT NULL,
+CREATE TABLE `fogorvos` (
+  `ID` int(8) NOT NULL,
+  `mobiltelefonszam` int(10) DEFAULT NULL,
+  `nev` varchar(50) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `email_cim` varchar(50) COLLATE utf8_hungarian_ci DEFAULT NULL,
   `szuletesi_datum` date DEFAULT NULL,
-  `korzetek_szama` int(3) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 --
 -- A tábla adatainak kiíratása `fogorvos_osszevon`
 --
 
-INSERT INTO `fogorvos_osszevon` (`fogorvos_azon`, `Mobiltelefonszam`, `Neve`, `Email_cim`, `szuletesi_datum`, `korzetek_szama`) VALUES
-(12023, 620322101, 'Dr. Kovács Dániel', 'drkovacsdaniel@email.hu', '1979-11-19', 3),
-(20012, 630200123, 'Dr. Szilágyi George', 'szilagyi.george@gmail.com', '1963-02-10', 2),
-(20020, 2147483647, 'Dr.Horváth Péter', 'horvath.peterdr@email.hu', '1980-03-20', 3),
-(20101, 630510240, 'dr. Balázs István', 'balazs.istvandr@gmail.com', '1969-10-10', 3),
-(20301, 620756101, 'Dr. Orbán Balázs', 'dr.orban.balazs@gmail.com', '1956-03-23', 1),
-(23440, 620200145, 'Dr. Kovács István', 'kovacsdoktor@gmail.com', '1965-03-15', 2),
-(23442, 620200145, 'Dr. Varga Dániel Balázs', 'varga.daniel.balazs@gmail.com', '1990-06-01', 2),
-(25362, 620200145, 'Dr. Vajda Antónia Ágota', 'vajdaantonia@gmail.com', '1990-05-03', 4),
-(34310, 630250120, 'Dr. Kálócz Borbála', 'kalocz.borbala@gmail.com', '1974-10-23', 3),
-(70032, 647525200, 'Dr. Ugrai György', 'gyorgyugrai@gmail.com', '1989-03-04', 3);
+INSERT INTO `fogorvos` (`fogorvos_azon`, `Mobiltelefonszam`, `Neve`, `Email_cim`, `szuletesi_datum`) VALUES
+(1, 620322101, 'Dr. Kovács Dániel', 'drkovacsdaniel@email.hu', '1979-11-19'),
+(2, 630200123, 'Dr. Szilágyi George', 'szilagyi.george@gmail.com', '1963-02-10'),
+(3, 2147483647, 'Dr.Horváth Péter', 'horvath.peterdr@email.hu', '1980-03-20'),
+(4, 630510240, 'dr. Balázs István', 'balazs.istvandr@gmail.com', '1969-10-10'),
+(5, 620756101, 'Dr. Orbán Balázs', 'dr.orban.balazs@gmail.com', '1956-03-23'),
+(6, 620200145, 'Dr. Kovács István', 'kovacsdoktor@gmail.com', '1965-03-15'),
+(7, 620200145, 'Dr. Varga Dániel Balázs', 'varga.daniel.balazs@gmail.com', '1990-06-01'),
+(8, 620200145, 'Dr. Vajda Antónia Ágota', 'vajdaantonia@gmail.com', '1990-05-03'),
+(9, 630250120, 'Dr. Kálócz Borbála', 'kalocz.borbala@gmail.com', '1974-10-23'),
+(10, 647525200, 'Dr. Ugrai György', 'gyorgyugrai@gmail.com', '1989-03-04');
 
 --
 -- Indexek a kiírt táblákhoz
