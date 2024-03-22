9927786146a1ea5acf2397cbddbfad13712189f9
bencepapp09 <112862119+bencepapp09@users.noreply.github.com>
2024-03-22 09:05:32 +0100

    Update orvosok.sql

--- adatbázis/orvosok.sql
+++ adatbázis/orvosok.sql
@@ -34,22 +34,19 @@ CREATE TABLE `orvosok` (
   `iranyitoszam` int(11) DEFAULT NULL,
   `cim` text DEFAULT NULL,
   `telefon` int(11) DEFAULT NULL,
-  `egyalkalmas_jelszo` int(11) DEFAULT NULL,
-  `letrehoz_datuma` date DEFAULT NULL,
-  `frissit_datuma` date DEFAULT NULL,
-  `varos` varchar(15) DEFAULT NULL
+  `egyalkalmas_jelszo` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
 -- A tábla adatainak kiíratása `orvosok`
 --
 
-INSERT INTO `orvosok` (`id`, `elso_nev`, `utolso_nev`, `iranyitoszam`, `cim`, `telefon`, `egyalkalmas_jelszo`, `letrehoz_datuma`, `frissit_datuma`) VALUES
-(1, 'dr.Bognár', 'Virág', 1014, 'Hegedűs Gyula utca 33.', 2147483647, 12345, '2023-06-19', '2023-06-26'),
-(2, 'dr.Balogh', 'Orsolya', 1017, 'Szent István körút 18.', 620220410, 23412, '2023-05-21', '2023-05-28'),
-(3, 'dr.Szabó', 'Zoltán', 1030, 'Boldog Sándor István körút 1-3.', 620310200, 98764, '2023-06-21', '2023-06-28'),
-(4, 'dr.Tóth', 'Réka', 1017, 'Munkácsy Mihály utca 15.', 620320195, 64556, '2023-07-20', '2023-07-27'),
-(5, 'dr.Tóbiás', 'Dávid', 1017, 'Szent István körút 18.', 620320195, 23412, '2023-04-21', '2023-04-28');
+INSERT INTO `orvosok` (`id`, `nev`, `iranyitoszam`, `cim`, `telefon`, `egyalkalmas_jelszo`) VALUES
+(1, 'dr.Bognár Virág', 1014, 'Hegedűs Gyula utca 33.', 2147483647, 12345),
+(2, 'dr.Balogh Orsolya', 1017, 'Szent István körút 18.', 620220410, 23412),
+(3, 'dr.Szabó Zoltán', 1030, 'Boldog Sándor István körút 1-3.', 620310200, 98764),
+(4, 'dr.Tóth Réka', 1017, 'Munkácsy Mihály utca 15.', 620320195, 64556),
+(5, 'dr.Tóbiás Dávid', 1017, 'Szent István körút 18.', 620320195, 23412);
 
 --
 -- Indexek a kiírt táblákhoz
